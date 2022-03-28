c19590ad3f15077e34399212654ca0adb4ec94d6
James Newton-King <james@newtonking.com>
2022-03-28 16:31:50 +0800

    Simplify

--- GrpcShutdownTest/Program.cs
+++ GrpcShutdownTest/Program.cs
@@ -1,22 +1,42 @@
-using GrpcShutdownTest.Services;
-
 var builder = WebApplication.CreateBuilder(args);
-
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-
-// Add services to the container.
-builder.Services.AddGrpc();
 
 builder.Host.ConfigureHostOptions(options =>
 {
-    options.ShutdownTimeout = TimeSpan.FromSeconds(30);
+    options.ShutdownTimeout = TimeSpan.FromSeconds(15);
 });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-app.MapGrpcService<ShutdownTestService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+app.MapPost("/longrunning", async context =>
+{
+    app.Logger.LogInformation("Server long running started");
+
+    app.Logger.LogInformation("Server reading request");
+    var result = await context.Request.BodyReader.ReadAtLeastAsync(4);
+    context.Request.BodyReader.AdvanceTo(result.Buffer.End);
+
+    app.Logger.LogInformation("Server waiting a bit");
+    await Task.Delay(TimeSpan.FromSeconds(5));
+
+    var randomBytes = Enumerable.Range(1, 2_000_000).Select(i => (byte)i).ToArray();
+
+
+    var memory = context.Response.BodyWriter.GetMemory(randomBytes.Length);
+
+    app.Logger.LogInformation($"Server writing {randomBytes.Length} bytes response");
+    randomBytes.CopyTo(memory);
+
+    app.Logger.LogInformation($"Server advancing {randomBytes.Length} bytes response");
+    context.Response.BodyWriter.Advance(randomBytes.Length);
+});
+app.MapPost("/shutdown", context =>
+{
+    app.Logger.LogInformation("Server stopping application");
+
+    var hostApplicationLifetime = context.RequestServices.GetRequiredService<IHostApplicationLifetime>();
+    hostApplicationLifetime.StopApplication();
+    return Task.CompletedTask;
+});
 
 app.Run();
